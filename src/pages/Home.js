bb0648bce21354d6919a89b67724444192d3e35a
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-16 11:47:21 -0700

    Created sample Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// import Carousel from "../components/Carousel";
+
+export default function Home() {
+  return (
+    <body>
+      <img className="w-100" src="./assets/horizontal-pictures/rainbow-roll.jpg" alt="Rainbow Roll Sushi"/>
+      <p>
+        ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+        corporis iste aliquid nobis aspernatur vero! Maxime vero nesciunt, nobis
+        laborum placeat corporis ipsam. Rem dignissimos nam excepturi illo,
+        dolor velit.
+      </p>
+    </body>
+  );
+}

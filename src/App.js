14dea6b71b8d55f5e0223525b7e728ffe5bfa8b4
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-16 11:45:16 -0700

    added Routes and Page elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./layout/NavBar";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="">
+          <NavBar />
+        </header>
+        <Routes>
+          <Route path="/" />
+          <Route path="menu" element={<Menu />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="*" exact={true} element={<NoPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

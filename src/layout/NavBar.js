75c25b1d319df2aaaca65aeec739e63ed175f013
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-16 11:46:37 -0700

    Created NavBar component, dark variant

---
+++ src/layout/NavBar.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="">
+            <Nav.Link href="/menu">Menu</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    </>
+  );
+}
+
+export default NavBar;

7f2bacd3cb5017a4762c5b389ef808d974aa7814
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-16 19:36:30 -0700

    added props.price col centered

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { BsCartPlusFill } from "react-icons/bs";
 
 export default function MenuCard({ props }) {
   return (
@@ -8,9 +9,19 @@ export default function MenuCard({ props }) {
       <Card.Body className="flex-fill row align-items-end justify-content-center">
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>{props.desc}</Card.Text>
-        <Button variant="primary" className="col-6">
-          Go somewhere
-        </Button>
+        <div className="col">
+          <Card.Text className="d-flex align-items-end justify-content-center">
+            ${props.price}
+          </Card.Text>
+          <div className="row justify-content-center">
+            <Button
+              variant="primary"
+              className="col-6 d-flex align-items-center justify-content-around"
+            >
+              Add to Cart <BsCartPlusFill />
+            </Button>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );

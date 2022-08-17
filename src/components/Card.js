4cfc2b532b675e27d6d9ecd11b726500d1ba1364
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-16 18:36:35 -0700

    button half sized and centered

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,10 @@ export default function MenuCard({ props }) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={props.img} />
-      <Card.Body className="flex-fill row align-items-end">
+      <Card.Body className="flex-fill row align-items-end justify-content-center">
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>{props.desc}</Card.Text>
-        <Button variant="primary" className="col">
+        <Button variant="primary" className="col-6">
           Go somewhere
         </Button>
       </Card.Body>

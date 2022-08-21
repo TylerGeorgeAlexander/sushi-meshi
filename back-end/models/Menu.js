fdb1ff4955ab6cf2ec115417343e679a02fdc0ef
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-20 21:50:37 -0700

    Menu model with schema set up with dummy data ex

---
+++ back-end/models/Menu.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const MenuSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  desc: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Menu", MenuSchema);
+
+// {
+//     name: "Rainbow Roll",
+//     price: 11.99,
+//     img: "./assets/horizontal-pictures/rainbow-roll.jpg",
+//     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum voluptates voluptatibus maiores illum dicta animi debitis ad aperiam omnis molestias! Corporis corrupti iure sed ex eos? Ad accusantium minus a!",
+//   },

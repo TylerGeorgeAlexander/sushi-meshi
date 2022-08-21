4094689d95898cd9140e1d49d13e219e397bbd87
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-20 21:47:05 -0700

    express connected DB- app.use added menu routes

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+const todoRoutes = require('./routes/todos')
+const menuRoutes = require('./routes/menu')
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', homeRoutes)
+app.use('/todos', todoRoutes)
+app.use('/menu', menuRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    

5d6d37eca0563251944efe0da0372b0ff4947825
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-08-20 21:49:22 -0700

    added getMenu to controller with string test res

---
+++ back-end/controllers/menu.js
@@ -0,0 +1,54 @@
+const Menu = require('../models/Menu')
+
+module.exports = {
+    getMenu: async (req,res)=>{
+        try{
+            const menuItems = await Menu.find()
+            // const itemsLeft = await Todo.countDocuments({completed: false})
+            res.send("This is from express.js");
+        }catch(err){
+            console.log(err)
+        }
+    },
+    // createTodo: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false})
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
+    // markComplete: async (req, res)=>{
+    //     try{
+    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+    //             completed: true
+    //         })
+    //         console.log('Marked Complete')
+    //         res.json('Marked Complete')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
+    // markIncomplete: async (req, res)=>{
+    //     try{
+    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+    //             completed: false
+    //         })
+    //         console.log('Marked Incomplete')
+    //         res.json('Marked Incomplete')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
+    // deleteTodo: async (req, res)=>{
+    //     console.log(req.body.todoIdFromJSFile)
+    //     try{
+    //         await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+    //         console.log('Deleted Todo')
+    //         res.json('Deleted It')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // }
+}

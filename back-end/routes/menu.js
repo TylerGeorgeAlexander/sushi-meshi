c6301aa864a8c944ce365b3050ee61effb468ddc
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-06 00:54:17 -0700

    added POST / createMenuItem

--- back-end/routes/menu.js
+++ back-end/routes/menu.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const menuController = require('../controllers/menu')
 
+// @ Route /menu 
+// From server.js
+
 router.get('/', menuController.getMenu)
 
-// router.post('/createTodo', todosController.createTodo)
+router.post('/add', menuController.createMenuItem)
 
 // router.put('/markComplete', todosController.markComplete)
 

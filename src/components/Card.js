885ddea04d955335fbc15694649155077796e21b
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-16 19:04:32 -0700

    passed props properly and used beta.react.docs

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,34 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { BsCartPlusFill } from "react-icons/bs";
 
-export default function MenuCard({ props }) {
+export default function MenuCard({ dataElement, cart, setCart }) {
+  // console.log(dataElement);
+
   return (
     <Card className="box-shadow m-2" style={{ width: "18rem" }}>
+      <p>{JSON.stringify(dataElement)}</p>
       <Card.Img
         className="card-image-top img-fluid p-2"
         variant="top"
-        src={props.img}
+        src={dataElement.img}
       />
       <Card.Body className="flex-fill row align-items-end justify-content-center">
-        <Card.Title>{props.name}</Card.Title>
-        <Card.Text>{props.desc}</Card.Text>
+        <Card.Title>{dataElement.name}</Card.Title>
+        <Card.Text>{dataElement.desc}</Card.Text>
         <div className="col">
           <Card.Text className="d-flex align-items-end justify-content-center">
-            ${props.price}
+            ${dataElement.price}
           </Card.Text>
           <div className="row justify-content-center">
             <Button
               variant="primary"
               className="col-6 d-flex align-items-center justify-content-around"
+              onClick={() =>
+                setCart({
+                  totalPrice: cart.totalPrice + dataElement.price,
+                  name: dataElement.name,
+                })
+              }
             >
               Add to Cart <BsCartPlusFill />
             </Button>

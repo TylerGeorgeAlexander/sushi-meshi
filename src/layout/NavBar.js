28574eb3d261568820f6d1a618d7f1b2169dbacc
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-16 19:23:04 -0700

    styling and props passed

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -2,12 +2,15 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function NavBar() {
+function NavBar({ cart }) {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand href="/">
+            Home
+            <Nav className="navbar-text text-white">${cart.totalPrice}</Nav>
+          </Navbar.Brand>
           <Nav className="">
             <Nav.Link href="/addtomenu">Add To Menu</Nav.Link>
             <Nav.Link href="/menu">Menu</Nav.Link>
